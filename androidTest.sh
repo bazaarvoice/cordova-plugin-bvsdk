1da193779c437ce9f63682160b3c83e95ddbeedd
Tanvir Pathan <tanvir.pathan@bazaarvoice.com>
2016-12-15 16:55:19 -0600

    Adding conditions to reporting

--- androidTest.sh
+++ androidTest.sh
@@ -4,5 +4,10 @@ cd bvdemo/platforms/android
 TESTDIR=$(pwd)
 echo Repository directory is: $TESTDIR
 calabash-android run $TESTDIR/build/outputs/apk/android-debug.apk --format json -o cucumber.json
-chmod 755 report_parser.py
-./report_parser.py
+echo report parser is $REPORT_PARSER
+if [ -z $REPORT_PARSER ]; then
+    echo Skipping report parser, android
+else
+    chmod 755 report_parser.py
+    ./report_parser.py
+fi
--- iosTest.sh
+++ iosTest.sh
@@ -5,5 +5,10 @@ xcodebuild -list
 carthage update --no-use-binaries
 xcodebuild -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO -scheme BVInterface-cal -project BVInterface.xcodeproj
 cucumber --tags ~@recommendations_test --format json -o cucumber.json
-chmod 755 report_parser.py
-./report_parser.py
+echo report parser is $REPORT_PARSER
+if [ -z $REPORT_PARSER ]; then
+    echo Skipping report parser, ios
+else
+    chmod 755 report_parser.py
+    ./report_parser.py
+fi
